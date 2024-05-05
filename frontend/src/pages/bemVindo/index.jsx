1be0a49d136d24976392380773c76fecde36f5f5
Edy-sl <edyslima.android@gmail.com>
2024-05-05 15:15:38 -0300

    04/05/2024

--- frontend/src/pages/bemVindo/index.jsx
+++ frontend/src/pages/bemVindo/index.jsx
@@ -4,7 +4,16 @@ import { FormFactoring } from '../../components/formFactoring';
 import { FormLogin } from '../../components/formLogin';
 import { apiFactoring } from '../../services/api';
 import { Calculadora } from '../../components/calculadora/calculadora';
+
+import {
+    FaCar,
+    FaRegMoneyBillAlt,
+    FaMoneyCheck,
+    FaAddressBook,
+} from 'react-icons/fa';
+import { FaChartColumn } from 'react-icons/fa6';
 import './bemVindo.css';
+import { Link } from 'react-router-dom';
 
 export const BemVindo = () => {
     const { autenticado, factoring } = useContext(AuthContext);
@@ -15,7 +24,39 @@ export const BemVindo = () => {
 
             {factoring !== 'null' && autenticado == true && (
                 <div id="divBemVindo">
-                    <h1>Seja bem vindo!</h1>
+                    <div id="divBotoesI">
+                        <Link to="cadastro-cliente">
+                            <div id="divBtnI">
+                                <FaRegMoneyBillAlt size="30" />
+                                Clientes
+                            </div>
+                        </Link>
+                        <Link to="emprestimo/dinheiro">
+                            <div id="divBtnI">
+                                <FaRegMoneyBillAlt size="30" />
+                                Empréstimo
+                            </div>
+                        </Link>
+                        <Link to="emprestimo/veiculo">
+                            <div id="divBtnI">
+                                <FaCar size="30" />
+                                Veículos
+                            </div>
+                        </Link>
+                        <Link to="/bordero">
+                            <div id="divBtnI">
+                                <FaMoneyCheck size="30" />
+                                Cheques
+                            </div>
+                        </Link>
+                        <Link to="/relatorio-movimento-emissao">
+                            <div id="divBtnI">
+                                <FaChartColumn size="30" />
+                                Movimentação
+                            </div>
+                        </Link>
+                    </div>
+
                     <div id="divAtalhos">
                         Atalhos<br></br>
                         F11 Tela Cheia <br></br>
